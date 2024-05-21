7d6d7f98339388ddd45e3fc84cf32abfbf88d842
Anas Sghir <anas.sghir@toulouse.miage.fr>
2024-05-21 14:23:42 +0200

    first project

---
+++ src/main/java/com/MIAGE/jeuxmiagiques/JeuxMiagiquesApplication.java
@@ -0,0 +1,13 @@
+package com.MIAGE.jeuxmiagiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuxMiagiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuxMiagiquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MIAGE/jeuxmiagiques/JeuxMiagiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MIAGE.jeuxmiagiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuxMiagiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
