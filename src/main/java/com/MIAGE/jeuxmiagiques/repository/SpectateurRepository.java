b59f7d864708627d3a43834df142c9687832fcec
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 02:06:03 +0200

    refactor: Add User model to SpectateurRepository

--- src/main/java/com/MIAGE/jeuxmiagiques/repository/SpectateurRepository.java
+++ src/main/java/com/MIAGE/jeuxmiagiques/repository/SpectateurRepository.java
@@ -3,5 +3,7 @@ package com.MIAGE.jeuxmiagiques.repository;
 import com.MIAGE.jeuxmiagiques.model.Spectateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.MIAGE.jeuxmiagiques.model.User;
+
 public interface SpectateurRepository extends JpaRepository<Spectateur, Integer> {
 }
