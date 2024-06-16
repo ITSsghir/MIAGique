a919196e5ff1212d7c043914275afb610eeaa131
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 17:46:01 +0200

    refactor: Update InfrastructureSportive model with getters and setters

--- src/main/java/com/MIAGE/jeuxmiagiques/model/InfrastructureSportive.java
+++ src/main/java/com/MIAGE/jeuxmiagiques/model/InfrastructureSportive.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 @Entity
 public class InfrastructureSportive {
     @Id
@@ -13,30 +14,33 @@ public class InfrastructureSportive {
     private String adresse;
     private int capacite;
 
-    // Getters
+    // Getters and Setters
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNom() {
         return nom;
     }
 
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public int getCapacite() {
-        return capacite;
-    }
-
-    // Setters
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    public String getAdresse() {
+        return adresse;
+    }
+
     public void setAdresse(String adresse) {
         this.adresse = adresse;
+    }
+
+    public int getCapacite() {
+        return capacite;
     }
 
     public void setCapacite(int capacite) {
