05f604bf124e1a8f32b13638f3fc844dd9d5e4c6
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 02:05:58 +0200

    refactor: Add password field to ParticipantById constructor

--- src/main/java/com/MIAGE/jeuxmiagiques/translationUnits/ParticipantById.java
+++ src/main/java/com/MIAGE/jeuxmiagiques/translationUnits/ParticipantById.java
@@ -1,18 +1,22 @@
 package com.MIAGE.jeuxmiagiques.translationUnits;
 
-public class ParticipantById {
+import com.MIAGE.jeuxmiagiques.model.User;
+
+public class ParticipantById extends User {
 
     private String nom;
     private String prenom;
     private String email;
+    private String password;
     private int delegationId;
 
     // Constructors
-    public ParticipantById(String nom, String prenom, String email, int delegationId) {
+    public ParticipantById(String nom, String prenom, String email, int delegationId, String password) {
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
         this.delegationId = delegationId;
+        this.password = password;
     }
 
     // Getters
@@ -31,6 +35,10 @@ public class ParticipantById {
 
     public int getDelegationId() {
         return delegationId;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     // Setters
