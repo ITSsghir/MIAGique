73957a82b94fc6318400bdc1e9da9959d4d50ec1
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 17:45:50 +0200

    refactor: Update JeuxMiagiquesApplication to include entity scan

--- src/main/java/com/MIAGE/jeuxmiagiques/JeuxMiagiquesApplication.java
+++ src/main/java/com/MIAGE/jeuxmiagiques/JeuxMiagiquesApplication.java
@@ -2,11 +2,12 @@ package com.MIAGE.jeuxmiagiques;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class JeuxMiagiquesApplication{
-
-	public static void main(String[] args) {
-		SpringApplication.run(JeuxMiagiquesApplication.class, args);
-	}
+@EntityScan(basePackages = "com.MIAGE.jeuxmiagiques.model")
+public class JeuxMiagiquesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JeuxMiagiquesApplication.class, args);
+    }
 }
